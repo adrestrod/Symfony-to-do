5ecf81abfba0efd70d31451aa14519fbcb3dfa26
Adrian Estevez <adrestrod123@gmail.com>
2021-10-07 12:33:54 +0200

    Corregindo fallo de renderizado de plantillas

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,10 +6,12 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class User implements UserInterface
 {
@@ -40,6 +42,16 @@ class User implements UserInterface
      * @ORM\OneToMany(targetEntity=Tarea::class, mappedBy="usuario", orphanRemoval=true)
      */
     private $tareas;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $ultimoAcceso;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __construct()
     {
@@ -166,5 +178,34 @@ class User implements UserInterface
         }
 
         return $this;
+    }
+
+    public function getUltimoAcceso(): ?\DateTimeInterface
+    {
+        return $this->ultimoAcceso;
+    }
+
+    public function setUltimoAcceso(?\DateTimeInterface $ultimoAcceso): self
+    {
+        $this->ultimoAcceso = $ultimoAcceso;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return "Testing";
     }
 }
